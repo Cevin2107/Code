f485a4fff8a1bc29587772d5d7a6a0fbe39c90fb
Cevin2107 <24022713@vnu.edu.vn>
2025-03-11 13:42:46 +0700

    Update code

--- Test1.cpp
+++ Test1.cpp
@@ -2,6 +2,16 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    int n;
+    cin >> n;
+    int arr[1000];
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 5 == 0) {
+            cout << arr[i] << " ";
+            count++;
+        }
+    }
+    cout << count;
     return 0;
 }
