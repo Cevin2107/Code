48a9d1b76522eca3f26ac4be376c895bb8cf6db2
Cevin2107 <24022713@vnu.edu.vn>
2025-03-11 13:38:05 +0700

    Update code 11/3/2025

--- Test1.cpp
+++ Test1.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int UCLN(int a, int b) {
-    if (b == 0)
-        return a;
-    return UCLN(b, a % b);
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
 }
